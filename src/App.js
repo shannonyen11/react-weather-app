c1a734f90cb4a77fcd60e75e854a6da0e620a197
shannon <shannonyen11@gmail.com>
2023-09-06 09:43:04 +0800

    Finish Realtime Weather App

--- src/App.js
+++ src/App.js
@@ -1,129 +1,94 @@
-import logo from './logo.svg';
+import { useState, useEffect, useMemo } from 'react';
 import styled from '@emotion/styled';
-import { ReactComponent as DayCloudyIcon} from './images/day-cloudy.svg';
-import { ReactComponent as AirFlowIcon} from './images/airFlow.svg'
-import { ReactComponent as RainIcon} from './images/rain.svg';
-import { ReactComponent as RefreshIcon} from './images/refresh.svg'
+import { ThemeProvider } from '@emotion/react';
+import { findLocation, getMoment } from './utils/helpers';
+
+import WeatherCard from './views/WeatherCard';
+import useWeatherAPI from './hooks/useWeatherAPI';
+import WeatherSetting from './views/WeatherSetting';
 
 
 const Container = styled.div`
-  background-color: #ededed;
+  background-color: ${({ theme }) => theme.backgroundColor};
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
-const WeatherCard = styled.div`
-  position: relative;
-  min-width: 360px;
-  box-shadow: 0 1px 3px 0 #999999;
-  background-color: #f9f9f9;
-  box-sizing: border-box;
-  padding: 30px 15px;
-`;
+const theme = {
+  light: {
+    backgroundColor: '#ededed',
+    foregroundColor: '#f9f9f9',
+    boxShadow: '0 1px 3px 0 #999999',
+    titleColor: '#212121',
+    temperatureColor: '#757575',
+    textColor: '#828282',
+  },
+  dark: {
+    backgroundColor: '#1F2022',
+    foregroundColor: '#121416',
+    boxShadow:
+      '0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)',
+    titleColor: '#f9f9fa',
+    temperatureColor: '#dddddd',
+    textColor: '#cccccc',
+  },
+};
 
-const Location = styled.div`
-  font-size: 28px;
-  color: #212121;
-  margin-bottom: 20px;
-`;
+const AUTHORIZATION_KEY = 'CWB-F1806D99-41B5-4AB0-B62C-47A76357E475';
 
-const Description = styled.div`
-  font-size: 16px;
-  color: #828282;
-  margin-bottom: 30px;
-`;
+function App() {  
+  const [currentCity, setCurrentCity] = useState(() => localStorage.getItem('cityName') || '高雄市');
+  const [currentTheme, setCurrentTheme] = useState('light');
+  const [currentPage, setCurrentPage] = useState('WeatherCard');
 
-const CurrentWeather = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 30px;
-`;
+  const handleCurrentPageChange = (currentPage) => {
+    setCurrentPage(currentPage);
+  };
 
-const Temperature = styled.div`
-  color: #757575;
-  font-size: 96px;
-  font-weight: 300;
-  display: flex;
-`;
+  const handleCurrentCityChange = (currentCity) => {
+    setCurrentCity(currentCity);
+  };
 
-const Celsius = styled.div`
-  font-weight: normal;
-  font-size: 42px;
-`;
+  const currentLocation = useMemo(() => findLocation(currentCity), [currentCity]);
 
-const AirFlow = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: #828282;
-  margin-bottom: 20px;
-  svg{
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
+  const {cityName, locationName, sunriseCityName} = currentLocation;
 
-const Rain = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: #828282;
-  svg{
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
+  const moment = useMemo(() => getMoment(sunriseCityName), []);
 
-const Refresh = styled.div`
-  position: absolute;
-  right: 15px;
-  bottom: 15px;
-  font-size: 12px;
-  display: inline-flex;
-  align-items: flex-end;
-  color: #828282;
-  svg{
-    width: 15px;
-    height: 15px;
-    margin-left: 10px;
-    cursor: pointer;
-  }
-`;
+  const [weatherElement, fetchData] = useWeatherAPI({
+    locationName,
+    cityName,
+    authorizationKey: AUTHORIZATION_KEY,
+  });
+  
+  useEffect(() => {
+    setCurrentTheme(moment === 'day' ? 'light' : 'dark');
+  }, [moment]);
 
-const DayCloudy = styled(DayCloudyIcon)`
-  flex-basis: 30%;
-`;
-
-function App() {
   return (
-    <Container>
-      <WeatherCard>
-        <Location>台北市</Location>
-        <Description>多雲時晴</Description>
-        <CurrentWeather>
-          <Temperature>
-            23 <Celsius>°C</Celsius>
-          </Temperature>
-          <DayCloudy />
-        </CurrentWeather>
-        <AirFlow>
-           <AirFlowIcon />23 m/h
-        </AirFlow>
-        <Rain>
-          <RainIcon />48%
-        </Rain>
-        <Refresh>
-          最後觀測時間：上午 12:03 <RefreshIcon />
-        </Refresh>
-      </WeatherCard>
-    </Container>
+    <ThemeProvider theme={theme[currentTheme]}>
+      <Container>
+        {currentPage === 'WeatherCard' && (
+          <WeatherCard 
+            cityName = {cityName}
+            weatherElement = {weatherElement}
+            moment = {moment}
+            fetchData = {fetchData}
+            handleCurrentPageChange = {handleCurrentPageChange}
+          />
+        )}
+
+        {currentPage === 'WeatherSetting' && (
+          <WeatherSetting 
+            cityName = {cityName}
+            handleCurrentCityChange = {handleCurrentCityChange}
+            handleCurrentPageChange = {handleCurrentPageChange} 
+          />
+        )}
+      </Container>
+    </ThemeProvider>    
   );
 }
 
